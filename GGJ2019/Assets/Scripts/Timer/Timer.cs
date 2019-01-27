8bfedc903fa13ccd9ca53e98f40da6edb18b7d10
AUdrey Tribillac <audrey.tribillac@epitech.eu>
2019-01-27 12:35:00 +0100

    add textBox radio

--- GGJ2019/Assets/Scripts/Timer/Timer.cs
+++ GGJ2019/Assets/Scripts/Timer/Timer.cs
@@ -12,14 +12,14 @@ public class Timer : MonoBehaviour {
     public Text time;
     public Image textbox;
     public Text text;
+    public GameObject GameManager;
+    public Init _init;
 
     private void Start()
     {
-        textbox = GetComponentInChildren<Image>();
-        text = textbox.GetComponentInChildren<Text>();
-
-        time = GetComponentInChildren<Text>();
         time.text = "0.0";
+        _init = GameManager.GetComponent<Init>();
+
     }
     // Update is called once per frame
     void Update () {
@@ -27,25 +27,59 @@ public class Timer : MonoBehaviour {
     }
 
 	void LaunchBigTimer() {
-		timeLeft -= Time.deltaTime;
-        time.text = (int)Mathf.Round(timeLeft) / 60 + ":" + Mathf.Round(timeLeft) % 60;
-		if ((int)timeLeft == 70)
+
+        if ((int)timeLeft == 90)
         {
             textbox.gameObject.SetActive(true);
-            text.text = "Info1";
+            text.text = "The hunter... Is hungry";
         }
-		if ((int)timeLeft == 40)
+        if ((int)timeLeft == 85)
+        {
+            textbox.gameObject.SetActive(false);
+        }
+        timeLeft -= Time.deltaTime;
+        time.text = "0" +(int)Mathf.Round(timeLeft) / 60 + ":" + Mathf.Round(timeLeft) % 60;
+
+        if ((int)timeLeft == 70)
+        {
+            text.text = "Put the bird's cage closer to the ground, they seem to be happier";
+            textbox.gameObject.SetActive(true);
+        }
+        if ((int)timeLeft == 65)
+        {
+            textbox.gameObject.SetActive(false);
+        }
+        if ((int)timeLeft == 40)
         {
             textbox.gameObject.SetActive(true);
-            text.text = "Info2";
+
+            text.text = "Survey proves that bed are the worst item of the year!";
         }
-		if ((int)timeLeft == 10)
+        if ((int)timeLeft == 35)
+        {
+            textbox.gameObject.SetActive(false);
+        }
+        if ((int)timeLeft == 10)
         {
             textbox.gameObject.SetActive(true);
-            text.text = "Info3";
+            text.text = "In comparison, the same survey proves that armor are a lot more reliable";
         }
-        if ((int)timeLeft == 0) {
+        if ((int)timeLeft == 5)
+        {
+            textbox.gameObject.SetActive(false);
+        }
+        if ((int)timeLeft <= 0) {
     		TimerEnd = true;
+            time.text = "00.00";
+            stopAll();
         }
 	}
+
+    public void stopAll()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            _init.all.juifs[i].button.interactable = false;
+        }
+    }
 }
