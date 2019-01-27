c940614b5a083e0736521a1e692dfa35d94c01a8
AUdrey Tribillac <audrey.tribillac@epitech.eu>
2019-01-27 11:40:31 +0100

    Modif assets scripts for timers

---
+++ GGJ2019/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+
+	public bool TimerEnd = false;
+	public float timeLeft = 90f;
+     
+    public Text time;
+    public Image textbox;
+    public Text text;
+
+    private void Start()
+    {
+        textbox = GetComponentInChildren<Image>();
+        text = textbox.GetComponentInChildren<Text>();
+
+        time = GetComponentInChildren<Text>();
+        time.text = "0.0";
+    }
+    // Update is called once per frame
+    void Update () {
+		LaunchBigTimer();
+    }
+
+	void LaunchBigTimer() {
+		timeLeft -= Time.deltaTime;
+        time.text = (int)Mathf.Round(timeLeft) / 60 + ":" + Mathf.Round(timeLeft) % 60;
+		if ((int)timeLeft == 70)
+        {
+            textbox.gameObject.SetActive(true);
+            text.text = "Info1";
+        }
+		if ((int)timeLeft == 40)
+        {
+            textbox.gameObject.SetActive(true);
+            text.text = "Info2";
+        }
+		if ((int)timeLeft == 10)
+        {
+            textbox.gameObject.SetActive(true);
+            text.text = "Info3";
+        }
+        if ((int)timeLeft == 0) {
+    		TimerEnd = true;
+        }
+	}
+}
---
+++ GGJ2019/Assets/Scripts/Timer/movingCursorObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingCursorObject : MonoBehaviour {
+	float x, y, z = 0;
+	int SpeedExposant = 150;
+	int PowerBar_size = 100;
+	bool right = true;
+	bool Space = false;
+
+	enum xColor {
+		GREEN,
+		ORANGE,
+		RED
+	};
+
+    int Update()
+    {
+		float tmpX = 0;
+		if (Input.GetKeyUp("space"))
+			Space = true;
+		if (!Space) {
+			if (right) {
+				tmpX = SpeedExposant*Time.deltaTime;
+				x += tmpX;
+				transform.position = new Vector3(transform.position.x + tmpX, transform.position.y, transform.position.z);
+			} else {
+				tmpX = -SpeedExposant*Time.deltaTime;
+				x += tmpX;
+				transform.position = new Vector3(transform.position.x + tmpX, transform.position.y, transform.position.z);
+			}
+			if (x >= PowerBar_size / 2)
+				right = false;
+			else if (x <= -PowerBar_size / 2)
+				right = true;
+		}
+		return (int)x;
+		//return chooseColor((int)x);
+	}
+
+	/*int chooseColor(int x)
+	{
+		xColor UIColor;
+
+		if (x > -5 && x < 5) {
+			UIColor = xColor.GREEN;
+			Debug.Log("GREEN");
+		 } else if ((x > -20 && x < -5) || (x > 5 && x < 20)) {
+			UIColor = xColor.ORANGE;
+			Debug.Log("ORANGE");
+		} else if ((x > -50 && x < -20) || (x > 20 && x < 50)) {
+			UIColor = xColor.RED;
+			Debug.Log("RED");
+		}
+		return (int)UIColor;
+	} */
+}
