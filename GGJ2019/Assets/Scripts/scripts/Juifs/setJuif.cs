85ad0378ace07c9cbafea060dd13ebfc22f952d7
AUdrey Tribillac <audrey.tribillac@epitech.eu>
2019-01-27 01:30:40 +0100

    add scripts juifs to select and unselect

---
+++ GGJ2019/Assets/Scripts/scripts/Juifs/setJuif.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class Sticker
+{
+    public Sprite found;
+    public Sprite disable;
+    public Sprite enable;
+    public Button _button;
+    public string time;
+    public bool select;
+    public bool hasJuif;
+    public bool fd;
+    public bool canHide;
+    public int indexJuif;
+
+}
+
+
+public class setJuif : MonoBehaviour
+{
+    public GameObject GameManager;
+    public Sticker _sticker;
+    public Init _init;
+
+    void removeJuif()
+    {
+        if (_init.all.juifs[_sticker.indexJuif].button.interactable == false && _sticker.hasJuif == true)
+        {
+            // change boolean values
+            _sticker.canHide = true;
+            _sticker.hasJuif = false;
+            _init.all.juifs[_sticker.indexJuif].hide = false;
+            _init.all.juifs[_sticker.indexJuif].select = false;
+
+            // enable button juif
+            _init.all.juifs[_sticker.indexJuif].button.interactable = true;
+
+            // change sprites
+            _init.all.juifs[_sticker.indexJuif].button.image.sprite = _init.all.juifs[_sticker.indexJuif].sprite;
+            _sticker._button.image.sprite = _sticker.enable;
+        }
+
+    }
+
+    void HideJuif()
+    {
+
+        // on button click hide juif
+        for (int i = 0; i < _init.all.juifs.Count; i++)
+        {
+            if (_init.all.juifs[i].select == true && _sticker.hasJuif == false)
+            {
+                //launch jauge + timer (KEVIN)
+
+                // change boolean values
+                _sticker.canHide = false;
+                _sticker.hasJuif = true;
+                _init.all.juifs[i].select = false;
+                _init.all.juifs[i].hide = true;
+
+                // disable button juif
+                _init.all.juifs[i].button.interactable = false;
+
+                // change sprites
+                _init.all.juifs[i].button.image.sprite = _init.all.juifs[i].onHide;
+                _sticker._button.image.sprite = _init.all.juifs[i].sticker;
+
+                // set index juif selected
+                _sticker.indexJuif = i;
+                return;
+            }
+        }
+
+        // on button click remove juif
+        removeJuif();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _init = GameManager.GetComponent<Init>();
+        _sticker.select = false;
+        _sticker.hasJuif = false;
+        _sticker.fd = false;
+        _sticker.canHide = true;
+        _sticker.indexJuif = 0;
+        _sticker._button.image.sprite = _sticker.enable;
+        _sticker._button.onClick.AddListener(() => HideJuif());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
---
+++ GGJ2019/Assets/Scripts/scripts/Nazi/move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject GameManager;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init file = GameManager.GetComponent<Init>();
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(file.all);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move_h = Input.GetAxis("Horizontal");
+        float move_v = Input.GetAxis("Vertical");
+
+
+
+        Vector3 move = new Vector3(move_h, 0.0f, move_v);
+        rb.AddForce(move * 10);
+    }
+}
